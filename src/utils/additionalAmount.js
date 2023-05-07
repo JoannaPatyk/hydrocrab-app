e6146b0a51f0d2c66b89f4e406c3d7f416c93e65
Joanna Patyk <patykjoanna93@gmail.com>
2023-05-07 14:51:17 +0200

    Changing possible options in settings

---
+++ src/utils/additionalAmount.js
@@ -0,0 +1,15 @@
+const additionalAmount = [
+    { value: '-', label: '-' },
+    { value: '200', label: '200' },
+    { value: '400', label: '400' },
+    { value: '600', label: '600' },
+    { value: '800', label: '800' },
+    { value: '1000', label: '1000' },
+    { value: '1200', label: '1200' },
+    { value: '1400', label: '1400' },
+    { value: '1600', label: '1600' },
+    { value: '1800', label: '1800' },
+    { value: '2000', label: '2000' }
+];
+
+export default additionalAmount;
